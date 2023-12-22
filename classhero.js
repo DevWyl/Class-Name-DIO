2c9c013aa692e32cbc11fa89821060dd30032e37
DevWyl <wylkerson03@gmail.com>
2023-12-21 23:37:50 -0300

    Class Hero

---
+++ classhero.js
@@ -0,0 +1,34 @@
+class hero {
+    nome;
+    idade;
+    tipo;
+
+    constructor(idade, tipo, name) {
+        this.name = name;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+    attack(){
+    let ataque ;
+
+    switch (this.tipo) {
+        case "mago":
+            ataque = "magia";
+            break;
+        case "guerreiro":
+            ataque = "espada";
+            break;
+        case "monge":
+            ataque = "artes marciais"
+            break;
+        case "ninja":
+            ataque = "shurinken"
+            break;
+        default:
+    } 
+    console.log(`O ${this.tipo} atacou usando ${ataque}`)
+    }
+}
+
+let heroi = new hero(23, "mago");
+heroi.attack();
